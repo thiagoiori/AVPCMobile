47fcdff32d5116a056d7a08271d0e1bc6c3f1cf1
thiagoiori <thiagoiori@gmail.com>
2017-05-17 15:57:08 +0200

    - Included Login and Forgot Password screens

--- app/src/main/java/com/avpc/avpcmobile/MainActivity.java
+++ app/src/main/java/com/avpc/avpcmobile/MainActivity.java
@@ -1,13 +1,53 @@
 package com.avpc.avpcmobile;
 
+import android.content.Context;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
-public class MainActivity extends AppCompatActivity {
+import com.avpc.avpcmobile.login.ForgotPasswordFragment;
+import com.avpc.avpcmobile.login.LoginFragment;
+
+public class MainActivity extends AppCompatActivity implements
+        LoginFragment.OnForgotPasswordListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LoginFragment loginFragment = new LoginFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.loginContainer, loginFragment).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.forgotpasswordmenu, menu);
+        return true;
+    }
+
+    @Override
+    public void onForgotPasswordClicked(String email) {
+        ForgotPasswordFragment forgotPasswordFragment = new ForgotPasswordFragment();
+        Bundle args = new Bundle();
+        args.putString("EMAIL", email);
+        forgotPasswordFragment.setArguments(args);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack so the user can navigate back
+        transaction.replace(R.id.loginContainer, forgotPasswordFragment);
+        transaction.addToBackStack(null);
+
+        // Commit the transaction
+        transaction.commit();
+
     }
 }
---
+++ app/src/main/java/com/avpc/avpcmobile/login/LoginFragment.java
@@ -0,0 +1,154 @@
+package com.avpc.avpcmobile.login;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.avpc.avpcmobile.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LoginFragment extends Fragment {
+
+    private OnForgotPasswordListener mListener;
+
+    private EditText txtEmail;
+    private EditText txtPassword;
+    private Button btnLogin;
+    private Button btnForgotPassword;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        txtEmail = (EditText) view.findViewById(R.id.txtEmail);
+        txtPassword = (EditText) view.findViewById(R.id.txtPassword);
+        btnLogin = (Button) view.findViewById(R.id.btnLogin);
+        btnForgotPassword = (Button) view.findViewById(R.id.btnForgotPassword);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+        btnForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onForgotPasswordClicked();
+            }
+        });
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                Log.d("TAG", "onAuthStateChanged:initializing");
+                if (user != null) {
+                    // User is signed in
+                    Log.d("TAG", "onAuthStateChanged:signed_in:" + user.getUid());
+                    //txtUser.setText(user.getEmail());
+                } else {
+                    // User is signed out
+                    Log.d("TAG", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnForgotPasswordListener) {
+            mListener = (OnForgotPasswordListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    private void login() {
+        signIn(txtEmail.getText().toString(), txtPassword.getText().toString());
+    }
+
+    private void onForgotPasswordClicked() {
+        if (mListener != null && txtEmail != null){
+            mListener.onForgotPasswordClicked(txtEmail.getText().toString());
+        }
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //Log.d("signInUser", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("loginActivity", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //updateUI(user);
+                        } else {
+                            Log.w("signInUser", "signInWithEmail:failed", task.getException());
+                            Toast.makeText(getActivity(), R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    public interface OnForgotPasswordListener{
+        void onForgotPasswordClicked(String email);
+    }
+
+}
